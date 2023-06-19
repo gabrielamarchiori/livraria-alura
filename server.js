baba360b5043dd15e755e8c4b33c0e3d3d1b121b
Gabriela <gabrielacamarchiori@gmail.com>
2023-06-19 11:08:40 -0300

    aula 02 finalizada

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
-const http = require('http')
-const port = 3000
+import app from "./src/app.js"
 
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200, {'Content-Type': 'text/plain'})
-//     res.end('Curso de Node')
-// })
+const port = process.env.PORT || 3000
 
-// server.listen(port, () => {
-//     console.log(`Servidor escutando em http://localhost:${port}`)
-// })
-
-const rotas = {
-    '/' : 'Curso de Node',
-    '/livros': 'Entrei na pagina de livros',
-    '/autores': 'Listagem de autores',
-    '/editora': 'Pagina da editora',
-    '/sobre': 'Informacoes sobre o projeto'
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
-  })
-
-  server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
 })
